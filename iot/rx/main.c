c9d281edad6b6ed4ff4d9f57d4b2c10c36d455a1
duydl <duy.dole.00ece@gmail.com>
2026-02-07 14:24:12 +0100

    3.1

--- iot/rx/main.c
+++ iot/rx/main.c
@@ -17,7 +17,13 @@
 
 #define CUSTOM_SVC_UUID     0xff00
 #define CUSTOM_CHR_UUID     0xee00
-#define TARGET_NAME         "RIOT-IOT-TX"
+#define DEVICE_NAME_PREFIX  "RIOT-BLE-"
+#define DEVICE_NAME_MAX_LEN 31
+#ifndef RX_MAX_CONN
+#define MAX_CONN            4
+#else
+#define MAX_CONN            RX_MAX_CONN
+#endif
 
 typedef struct __attribute__((packed)) {
     uint16_t seq;
@@ -33,16 +39,30 @@ static ble_uuid16_t g_svc_uuid = BLE_UUID16_INIT(CUSTOM_SVC_UUID);
 static ble_uuid16_t g_chr_uuid = BLE_UUID16_INIT(CUSTOM_CHR_UUID);
 
 static uint8_t g_addr_type;
-static uint16_t g_conn_handle;
-static uint16_t g_chr_val_handle;
-static uint16_t g_chr_ccc_handle;
+typedef enum {
+    CONN_UNUSED = 0,
+    CONN_CONNECTING,
+    CONN_CONNECTED,
+} conn_state_t;
+
+typedef struct {
+    conn_state_t state;
+    uint16_t conn_handle;
+    uint16_t chr_val_handle;
+    uint16_t chr_ccc_handle;
+    ble_addr_t addr;
+    char name[DEVICE_NAME_MAX_LEN + 1];
+} conn_slot_t;
+
+static conn_slot_t g_conns[MAX_CONN];
+static uint8_t g_scanning;
 
 static void start_scan(void);
 
 static int discover_chr_cb(uint16_t conn_handle, const struct ble_gatt_error *error,
                            const struct ble_gatt_chr *chr, void *arg)
 {
-    (void)arg;
+    conn_slot_t *slot = (conn_slot_t *)arg;
     (void)error;
 
     if (chr == NULL) {
@@ -50,12 +70,16 @@ static int discover_chr_cb(uint16_t conn_handle, const struct ble_gatt_error *er
     }
 
     if (ble_uuid_cmp(&chr->uuid.u, &g_chr_uuid.u) == 0) {
-        g_chr_val_handle = chr->val_handle;
-        g_chr_ccc_handle = chr->val_handle + 1;
+        if (!slot) {
+            return 0;
+        }
+        slot->chr_val_handle = chr->val_handle;
+        slot->chr_ccc_handle = chr->val_handle + 1;
 
         uint16_t ccc_value = 0x0001;
-        printf("# RX: enable notify (ccc=%u)\n", g_chr_ccc_handle);
-        int rc = ble_gattc_write_flat(conn_handle, g_chr_ccc_handle,
+        printf("# RX: enable notify (ccc=%u, dev=%s)\n",
+               slot->chr_ccc_handle, slot->name);
+        int rc = ble_gattc_write_flat(conn_handle, slot->chr_ccc_handle,
                                       &ccc_value, sizeof(ccc_value), NULL, NULL);
         if (rc != 0) {
             printf("# RX: CCC write failed rc=%d\n", rc);
@@ -68,7 +92,7 @@ static int discover_chr_cb(uint16_t conn_handle, const struct ble_gatt_error *er
 static int discover_svc_cb(uint16_t conn_handle, const struct ble_gatt_error *error,
                            const struct ble_gatt_svc *service, void *arg)
 {
-    (void)arg;
+    conn_slot_t *slot = (conn_slot_t *)arg;
     (void)error;
 
     if (service == NULL) {
@@ -78,34 +102,160 @@ static int discover_svc_cb(uint16_t conn_handle, const struct ble_gatt_error *er
     ble_gattc_disc_all_chrs(conn_handle,
                             service->start_handle,
                             service->end_handle,
-                            discover_chr_cb, NULL);
+                            discover_chr_cb, slot);
     return 0;
+}
+
+static conn_slot_t *find_slot_by_addr(const ble_addr_t *addr)
+{
+    for (int i = 0; i < MAX_CONN; i++) {
+        if (g_conns[i].state != CONN_UNUSED &&
+            memcmp(g_conns[i].addr.val, addr->val, sizeof(addr->val)) == 0 &&
+            g_conns[i].addr.type == addr->type) {
+            return &g_conns[i];
+        }
+    }
+    return NULL;
+}
+
+static conn_slot_t *find_slot_by_handle(uint16_t handle)
+{
+    for (int i = 0; i < MAX_CONN; i++) {
+        if (g_conns[i].state != CONN_UNUSED &&
+            g_conns[i].conn_handle == handle) {
+            return &g_conns[i];
+        }
+    }
+    return NULL;
+}
+
+static int active_conn_count(void)
+{
+    int count = 0;
+    for (int i = 0; i < MAX_CONN; i++) {
+        if (g_conns[i].state != CONN_UNUSED) {
+            count++;
+        }
+    }
+    return count;
+}
+
+static int has_connecting(void)
+{
+    for (int i = 0; i < MAX_CONN; i++) {
+        if (g_conns[i].state == CONN_CONNECTING) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+static void clear_slot(conn_slot_t *slot)
+{
+    if (!slot) {
+        return;
+    }
+    memset(slot, 0, sizeof(*slot));
+    slot->state = CONN_UNUSED;
+}
+
+static conn_slot_t *alloc_slot(const ble_addr_t *addr,
+                               const uint8_t *name, uint8_t name_len)
+{
+    for (int i = 0; i < MAX_CONN; i++) {
+        if (g_conns[i].state == CONN_UNUSED) {
+            conn_slot_t *slot = &g_conns[i];
+            memset(slot, 0, sizeof(*slot));
+            slot->state = CONN_CONNECTING;
+            slot->addr = *addr;
+            uint8_t copy_len = name_len;
+            if (copy_len > DEVICE_NAME_MAX_LEN) {
+                copy_len = DEVICE_NAME_MAX_LEN;
+            }
+            memcpy(slot->name, name, copy_len);
+            slot->name[copy_len] = '\0';
+            return slot;
+        }
+    }
+    return NULL;
+}
+
+static int name_matches(const uint8_t *name, uint8_t name_len)
+{
+    const char *prefix = DEVICE_NAME_PREFIX;
+    size_t prefix_len = strlen(prefix);
+
+    if (!name || name_len <= prefix_len) {
+        return 0;
+    }
+    if (memcmp(name, prefix, prefix_len) != 0) {
+        return 0;
+    }
+
+    int saw_digit = 0;
+    for (size_t i = prefix_len; i < name_len; i++) {
+        char c = (char)name[i];
+        if (c >= '0' && c <= '9') {
+            saw_digit = 1;
+            continue;
+        }
+        if (c == '/' && saw_digit) {
+            if (i + 1 >= name_len) {
+                return 0;
+            }
+            for (i = i + 1; i < name_len; i++) {
+                c = (char)name[i];
+                if (c < '0' || c > '9') {
+                    return 0;
+                }
+            }
+            return 1;
+        }
+        return 0;
+    }
+    return saw_digit;
 }
 
 static int gap_event(struct ble_gap_event *event, void *arg)
 {
-    (void)arg;
+    conn_slot_t *slot = (conn_slot_t *)arg;
 
     switch (event->type) {
     case BLE_GAP_EVENT_CONNECT: {
         if (event->connect.status != 0) {
             printf("# RX: connect failed status=%d\n", event->connect.status);
+            if (slot) {
+                clear_slot(slot);
+            }
             start_scan();
             return 0;
         }
-        g_conn_handle = event->connect.conn_handle;
+        if (slot) {
+            slot->state = CONN_CONNECTED;
+            slot->conn_handle = event->connect.conn_handle;
+            printf("# RX: connected handle=%u dev=%s\n",
+                   slot->conn_handle, slot->name);
+        }
 
-        int rc = ble_gattc_disc_svc_by_uuid(g_conn_handle, &g_svc_uuid.u,
-                                            discover_svc_cb, NULL);
+        int rc = ble_gattc_disc_svc_by_uuid(event->connect.conn_handle,
+                                            &g_svc_uuid.u, discover_svc_cb,
+                                            slot);
         if (rc != 0) {
             printf("# RX: service discovery failed\n");
-            ble_gap_terminate(g_conn_handle, BLE_ERR_REM_USER_CONN_TERM);
+            ble_gap_terminate(event->connect.conn_handle,
+                              BLE_ERR_REM_USER_CONN_TERM);
         }
+        start_scan();
         return 0;
     }
 
     case BLE_GAP_EVENT_DISCONNECT:
         printf("# RX: disconnected reason=%d\n", event->disconnect.reason);
+        if (slot) {
+            clear_slot(slot);
+        } else {
+            clear_slot(find_slot_by_handle(event->disconnect.conn.conn_handle));
+        }
         start_scan();
         return 0;
 
@@ -119,7 +269,12 @@ static int gap_event(struct ble_gap_event *event, void *arg)
         sample_t sample;
         os_mbuf_copydata(event->notify_rx.om, 0, sizeof(sample), &sample);
 
-        printf("%u,%d,%d,%d,%d,%d,%d\n",
+        if (!slot) {
+            slot = find_slot_by_handle(event->notify_rx.conn_handle);
+        }
+        const char *dev_name = slot ? slot->name : "unknown";
+        printf("%s,%u,%d,%d,%d,%d,%d,%d\n",
+               dev_name,
                sample.seq,
                sample.temp_val, sample.temp_scale,
                sample.hum_val, sample.hum_scale,
@@ -141,6 +296,7 @@ static int scan_event(struct ble_gap_event *event, void *arg)
 
     switch (event->type) {
     case BLE_GAP_EVENT_DISC_COMPLETE:
+        g_scanning = 0;
         start_scan();
         return 0;
 
@@ -159,18 +315,34 @@ static int scan_event(struct ble_gap_event *event, void *arg)
 
         int name_match = 0;
         if (fields.name != NULL && fields.name_len > 0) {
-            if (strlen(TARGET_NAME) == fields.name_len &&
-                memcmp(fields.name, TARGET_NAME, fields.name_len) == 0) {
+            if (name_matches(fields.name, fields.name_len)) {
                 name_match = 1;
             }
         }
 
         if (uuid_match && name_match) {
-            printf("# RX: found %.*s, connecting...\n",
-                   fields.name_len, fields.name);
+            if (active_conn_count() >= MAX_CONN) {
+                return 0;
+            }
+            if (find_slot_by_addr(&event->disc.addr)) {
+                return 0;
+            }
+            conn_slot_t *slot = alloc_slot(&event->disc.addr,
+                                           fields.name,
+                                           fields.name_len);
+            if (!slot) {
+                return 0;
+            }
+            printf("# RX: found %s, connecting...\n", slot->name);
             ble_gap_disc_cancel();
-            ble_gap_connect(g_addr_type, &(event->disc.addr), 100,
-                            NULL, gap_event, NULL);
+            g_scanning = 0;
+            int rc = ble_gap_connect(g_addr_type, &(event->disc.addr), 100,
+                                     NULL, gap_event, slot);
+            if (rc != 0) {
+                printf("# RX: connect start failed rc=%d\n", rc);
+                clear_slot(slot);
+                start_scan();
+            }
         }
         return 0;
     }
@@ -180,11 +352,22 @@ static int scan_event(struct ble_gap_event *event, void *arg)
 
 static void start_scan(void)
 {
+    if (g_scanning) {
+        return;
+    }
+    if (has_connecting()) {
+        return;
+    }
+    if (active_conn_count() >= MAX_CONN) {
+        return;
+    }
     const struct ble_gap_disc_params scan_params = { 10000, 200, 0, 0, 0, 1 };
     int rc = ble_gap_disc(g_addr_type, 100, &scan_params, scan_event, NULL);
     if (rc != 0) {
         printf("# RX: scan failed rc=%d\n", rc);
+        return;
     }
+    g_scanning = 1;
 }
 
 int main(void)
@@ -194,7 +377,7 @@ int main(void)
     rc = ble_hs_id_infer_auto(0, &g_addr_type);
     assert(rc == 0);
 
-    printf("seq,temp_val,temp_scale,hum_val,hum_scale,press_val,press_scale\n");
+    printf("device,seq,temp_val,temp_scale,hum_val,hum_scale,press_val,press_scale\n");
 
     start_scan();
 
